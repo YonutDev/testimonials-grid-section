3132c3d06aa38fbcb00561acb910d56f3dbb4b16
Ionut Patrascu <patrascuionut456@gmail.com>
2022-12-05 22:16:46 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// Coded by yonutdev.xyz
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import './App.css'
+import Testimonials from './Testimonials'
+
+function App() {
+  return <Testimonials />
+}
+
+export default App;
